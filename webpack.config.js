eb4a3b378ac1d4818d1b899410b2c81e969f8173
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-12 00:44:28 -0400

    fixed font-woff loader. incorrect spelling

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,8 @@ module.exports = {
                 }
             }, 
             {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9]?$/, 
-                use: {
-                    loader: "url-loader?limit=10000imetype=application/font-woff"
-                }
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                use: { loader: "url-loader?limit=10000&mimetype=application/font-woff" }
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
