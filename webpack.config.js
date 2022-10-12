109a4e7682e95356f66003df584b7047eb40ca4c
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-12 00:14:51 -0400

    added rule for js and jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,13 @@ let HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: "./app/index.js",
     module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                excludes: /node_modules/,
+                use: ['babel-loader'],
+            },
+            
+        ]
     }
 }
