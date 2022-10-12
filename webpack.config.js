18eeed6427a1e44932304557a795b0e696876b98
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-12 00:25:34 -0400

    added last 2 rules for modules

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,19 @@ module.exports = {
                     loader: "url-loader?limit=100000"
                 }
             }, 
-            
-
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9]?$/, 
+                use: {
+                    loader: "url-loader?limit=10000imetype=application/font-woff"
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: {
+                    loader: "file-loader"
+                }
+            }
         ]
-    }
+    },
+    
 }
